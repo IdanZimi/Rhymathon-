d42b5275be8e56ad3bbe9b90d7b32fab5a44b2e1
IdanZimi <idanz27@gmail.com>
2018-11-13 17:00:57 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router  , Link} from 'react-router-dom'
+import './App.css'
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div>
+            <h1 id="my">My Rhymathon</h1>
+            <button type="button" class="btn btn-secondary btn-lg btn-block button"><span className="enterSpan"><Link to="/mainPage">Enter</Link></span></button>
+          </div>
+
+          <div className="main-routes">
+            {/* <Route path="/mainPage" exact component={mainPage} /> */}
+
+          </div>
+        </div>
+      </Router>
     );
   }
 }

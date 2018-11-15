2c58e5442118d67de731c1e5c9f782e20991e5b5
neomie peretz <neoperetzmie@hotmail.com>
2018-11-15 13:36:56 +0200

    search

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -9,6 +9,7 @@ class mainPage extends Component {
     this.state = {
       lines: [],
       word: '',
+      saved: false
     }
   }
 
@@ -27,6 +28,14 @@ class mainPage extends Component {
     this.setState({ word: e.target.value })
   }
 
+  removePoem(){
+    this.setState({saved:true})
+  }
+
+  addPoem(){
+    this.setState({saved:false})
+  }
+
   getRhymes = async () => {
     let result = await axios.get('https://api.datamuse.com/words?rel_rhy=' + this.state.word)
     //random rhymes
@@ -34,7 +43,10 @@ class mainPage extends Component {
     console.log(rhymes)
     const lines = []
     let i = 0
-    if(rhymes[0]===undefined){
+    if(this.state.word === ''){
+      return alert("Please fill in a word.")
+    }
+    else if(rhymes[0]===undefined){
       return alert('This word does not rhyme with any currently known word.')
     }
     while (i < 4) {
@@ -45,13 +57,14 @@ class mainPage extends Component {
       }
     }
     this.setState({ lines: lines })
+    addPoem()
   }
 
   render() {
 
-    if (this.state.lines[0] === undefined) {
+    if (this.state.lines[0] === undefined || this.state.saved===true) {
       return (
-      <div className="mainPage">
+      <div className="mainPage backMainPage">
         <div className="word-search">
           <input className="wordInput" type="text" value={this.state.word} onChange={this.updateWord} placeholder="Choose a word" />
           <button className="button" type="button" onClick={this.getRhymes}><span>Go</span></button>
@@ -60,7 +73,7 @@ class mainPage extends Component {
     }
     else {
       return (
-        <div className="mainPage">
+        <div className="mainPage backMainPage">
           <div className="word-search">
             <input className="wordInput" type="text" value={this.state.word} onChange={this.updateWord} placeholder="Choose a word" />
             <button className="button" type="button" onClick={this.getRhymes}><span>Go</span></button>

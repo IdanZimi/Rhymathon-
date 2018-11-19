75552f780d1ac9e975c847ae67244242db88ca0f
neomie peretz <neoperetzmie@hotmail.com>
2018-11-19 16:00:13 +0200

    'profile'

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import '../Css/profilePage.css'
+import MyPoem from './MyPoem';
+class ProfilePage extends Component {
+    constructor() {
+        super()
+        this.state = {
+            user: {}
+        }
+    }
+
+    componentWillMount() {
+        let myUser = JSON.parse(localStorage.getItem("user") || "{}")
+        this.setState({ user: myUser })
+        console.log(myUser)
+    }
+
+    render() {
+        
+        if (this.state.user.poems[0] === undefined) {
+            return (
+                <div className="profilepage">
+                    <h1>{this.state.user.userName}({this.state.user.firstName} {this.state.user.lastName})</h1>
+                    <h4>Poems</h4>
+                    <p>You haven't written any poems yet.</p>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className="profilepage">
+                    <h1>{this.state.user.userName}({this.state.user.firstName} {this.state.user.lastName})</h1>
+                    <h4>Poems</h4>
+                    <p>{this.state.user.poems.map((poem) => {
+                        return <MyPoem poem = {poem} />
+                    })}</p>    
+                </div>
+            );
+        }
+    }
+}
+
+export default ProfilePage;
---
+++ src/components/myPoem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class MyPoem extends Component {
+
+    render() {
+        return (
+            <div className="mypoem">
+                {this.props.poem.lyrics.map((line) => {
+                    if (line.id === this.props.poem.lyrics[this.props.poem.lyrics.length - 1].id) {
+                        return (<p>{line.text} {line.rhyme}.</p>)
+                    }
+                    return (<p>{line.text} {line.rhyme},</p>)
+                })}
+      </div>
+        );
+    }
+}
+
+export default MyPoem;
